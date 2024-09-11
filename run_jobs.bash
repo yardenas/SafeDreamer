f72a085d26ff19aac2ca475191c85df7dd477451
Yarden <yarden.as@inf.ethz.ch>
2024-09-11 22:17:48 +0200

    Run jobs

---
+++ run_jobs.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Array of tasks
+tasks=("go_to_goal" "push_box" "press_buttons")
+
+# Loop over tasks and seeds
+for task in "${tasks[@]}"; do
+  for seed in {0..5}; do
+    # Submit the job for each combination of task and seed
+    sbatch <<EOT
+#!/bin/bash
+#SBATCH --account=ls_krausea
+#SBATCH --job-name=Train_${task}_Seed${seed}      # Job name for each task/seed combination
+#SBATCH --output=train_${task}_seed${seed}_%j.log # Output log for each job
+#SBATCH --error=train_${task}_seed${seed}_%j.err  # Error log for each job
+#SBATCH --gpus=rtx_4090:1
+#SBATCH --cpus-per-task=10                         # Number of CPU cores
+#SBATCH --mem-per-cpu=10240
+#SBATCH --time=4:00:00                           # Time limit
+#SBATCH --requeue                                 # Automatically requeue job if it fails
+
+# Run the Python script for this task and seed
+python3 SafeDreamer/train.py \
+    --configs bsrp_lag \
+    --method bsrp_lag \
+    --jax.platform gpu \
+    --task safeadaptationgym_point_${task} \
+    --seed ${seed}
+EOT
+
+  done
+done
