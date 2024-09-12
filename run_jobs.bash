84f82d67509ca6206bb71975ed423d71fbb9239e
Yarden As <yarden.as@inf.ethz.ch>
2024-09-12 12:30:44 +0200

    make it work

--- run_jobs.bash
+++ run_jobs.bash
@@ -14,7 +14,7 @@ for task in "${tasks[@]}"; do
 #SBATCH --output=train_${task}_seed${seed}_%j.log # Output log for each job
 #SBATCH --error=train_${task}_seed${seed}_%j.err  # Error log for each job
 #SBATCH --gpus=rtx_2080:1
-#SBATCH --cpus-per-task=20                         # Number of CPU cores
+#SBATCH --cpus-per-task=10                         # Number of CPU cores
 #SBATCH --mem-per-cpu=10240
 #SBATCH --time=4:00:00                           # Time limit
 #SBATCH --requeue                                 # Automatically requeue job if it fails
@@ -27,7 +27,7 @@ python3 SafeDreamer/train.py \
     --task safeadaptationgym_point_${task} \
     --seed ${seed} \
     --run.steps 5e6 \
-    --envs.amount 10 \
+    --envs.amount 5 \
     --logdir /cluster/scratch/yardas/safedreamer/logdir_${task}_seed${seed}
 EOT
 
